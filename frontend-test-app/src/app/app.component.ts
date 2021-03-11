047afea71143093cc9eb370bda251549294ad73d
Gajdács Enikő <eniko.gajdacs@p92.hu>
2021-03-11 15:10:43 +0100

    Add bootstrap and fontawesome, create basic layout

--- frontend-test-app/src/app/app.component.ts
+++ frontend-test-app/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './_services/data.service';
+import { faHome, faSync } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend-test-app';
+export class AppComponent implements OnInit {
+  title: string;
+  country: string;
+  lastUpdatedBy: string;
+  lastUpdated: string;
+  body: string;
+
+  notifications: any;
+
+  home = faHome;
+  sync = faSync;
+
+  constructor(private dataService: DataService){}
+
+  ngOnInit(): void {
+    this.dataService.getItem().subscribe(data => {
+      console.log(data);
+      this.title = data.title;
+      this.country = data.country;
+      this.lastUpdated = data.lastUpdated;
+      this.lastUpdatedBy = data.lastUpdatedBy;
+      this.body = data.body;
+    });
+
+    this.dataService.getNotifications().subscribe(nots => {
+      console.log(nots);
+      this.notifications = nots;
+    })
+  }
 }
